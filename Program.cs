24d6953da14f49e26d9702c11199aa408d4b516c
J. Japa <42208849+MrJapa@users.noreply.github.com>
2022-02-09 22:48:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Override
+{
+    public class Klasse1
+    {
+        public virtual void PrintKlasse()
+        {
+            Console.WriteLine("Fra klasse 1");
+        }
+    }
+    public class Klasse2 : Klasse1
+    {
+        public override void PrintKlasse()
+        {
+            Console.WriteLine("Fra klasse 2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Klasse2 k2 = new Klasse2();
+            k2.PrintKlasse();
+            Klasse1 k1 = new Klasse1();
+            k1.PrintKlasse();
+        }
+    }
+}
